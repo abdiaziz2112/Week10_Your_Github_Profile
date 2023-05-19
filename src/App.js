19bd8d61421c8909bb2663fd782e946d814c9a93
abdiaziz2112 <maashaaabdiaziz@gmail.com>
2023-05-19 11:25:51 +0300

    V.0.0.1

--- src/App.js
+++ src/App.js
@@ -1,37 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-// Import the "useState" and "useEffect" hooks from React
-// Soo jiido "useState" iyo "useEffect"
-
-// Import the "MyProfile" component
-// Soo jiido "MyProfile" component-ka
-
-// Import the "axios" library
-// Soo jiido "axios" hoos
-
-import "./App.css";
+import MyProfile from './components/MyProfile';
 
 function App() {
+  const [profile, setProfile] = useState({});
+  const [followers, setFollowers] = useState([]);
+  const [following, setFollowing] = useState([]);
 
-  // Create 3 states, "profile", "followers", and "following"
-  // Samee 3 state, "profile", "followers", iyo "following"
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const profileResponse = await axios.get(
+          'https://api.github.com/users/abdiaziz2112'
+        );
+        const followersResponse = await axios.get(
+          'https://api.github.com/users/abdiaziz2112/followers'
+        );
+        const followingResponse = await axios.get(
+          'https://api.github.com/users/abdiaziz2112/following'
+        );
 
+        setProfile(profileResponse.data);
+        setFollowers(followersResponse.data);
+        setFollowing(followingResponse.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
 
-  // API For Profile = https://api.github.com/users/<your-github-username>
-  // API for Followers = https://api.github.com/users/<your-github-username>/followers
-  // API for Following = https://api.github.com/users/<your-github-username>/following
+    fetchData();
+  }, []);
 
-  // Use axios to fetch data from the API using the useEffect hook
-  // Halkaan isticmaal axios adigoo kasoo jiidanaayo waxaa u baahantahay API, useEffect hook-na isticmaal
-    
   return (
-    <div className="bg-white md:mx-auto rounded shadow-xl w-full md:w-1/2 overflow-hidden">
-      <div className="h-[140px] bg-gradient-to-r from-cyan-500 to-blue-500"></div>
-
-      {/* Show "MyProfile" component here and give it 3 props, "profile", "followers", and "following" */}
-      {/* Halkaan soo gali "MyProfile", 3 props-na sii, "profile", "followers", iyo "following" */}
-
-    </div>
+    <Router>
+      <div className="bg-white md:mx-auto rounded shadow-xl w-full md:w-1/2 overflow-hidden">
+        <div className="h-[140px] bg-gradient-to-r from-cyan-500 to-blue-500"></div>
+        <MyProfile profile={profile} followers={followers} following={following} />
+      </div>
+    </Router>
   );
 }
 
